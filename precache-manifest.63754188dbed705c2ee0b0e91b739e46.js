d4f2e4fd838c31699d24a45fca830119edde6010
Chayoud <chayoud.p@arms-thai.com>
2018-12-20 17:39:59 +0700

    Updates

---
+++ precache-manifest.63754188dbed705c2ee0b0e91b739e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7f132833bee5f8af0fc",
+    "url": "/react-reader-page/static/js/runtime~main.b7f13283.js"
+  },
+  {
+    "revision": "4ecdbd87b8a30b0acf3a",
+    "url": "/react-reader-page/static/js/main.4ecdbd87.chunk.js"
+  },
+  {
+    "revision": "e41921e5000c8e8955d3",
+    "url": "/react-reader-page/static/js/1.e41921e5.chunk.js"
+  },
+  {
+    "revision": "4ecdbd87b8a30b0acf3a",
+    "url": "/react-reader-page/static/css/main.9bb4e10e.chunk.css"
+  },
+  {
+    "revision": "e41921e5000c8e8955d3",
+    "url": "/react-reader-page/static/css/1.d3c3f855.chunk.css"
+  },
+  {
+    "revision": "c632c4a5660f25d6378d667d56295d9e",
+    "url": "/react-reader-page/index.html"
+  }
+];
